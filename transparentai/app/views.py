dafc202840d47c599f28bc788fb4ebb4b66efed5
Nathan <nathan.lauga@gmail.com>
2019-12-01 13:09:25 +0100

    Add: basic views for transparentai : 404, ai, gloassary and ressources

--- transparentai/app/views.py
+++ transparentai/app/views.py
@@ -4,8 +4,29 @@ from app import app
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    title = 'Home'
+    return render_template("index.html", title=title)
 
-# @app.errorhandler(404)
-# def not_found(e):
-#     return render_template("404.html")
+
+@app.route('/ai/', methods=['GET', 'POST'])
+def ai():
+    title = 'Create a new AI'
+    return render_template("ai.html", title=title)
+
+
+@app.route('/glossary/', methods=['GET', 'POST'])
+def glossary():
+    title = 'Glossary'
+    return render_template("glossary.html", title=title)
+
+
+@app.route('/ressources/', methods=['GET', 'POST'])
+def ressources():
+    title = 'Ressources'
+    return render_template("ressources.html", title=title)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    title = 'Page not found'
+    return render_template("404.html", title=title)
