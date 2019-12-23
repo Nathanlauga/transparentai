2e97444ae97ff4939690c47ce0db663d0c8996ea
Nathan <nathan.lauga@gmail.com>
2019-12-23 11:34:41 +0100

    Add: handle translation (init flask babel)

--- scripts/update_questions_db.py
+++ scripts/update_questions_db.py
@@ -9,22 +9,31 @@ import sys
 
 import data
 
+
 def load_db():
-    parent_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    parent_path = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), os.pardir))
     return TinyDB(parent_path+'/transparentai/app/db/db.json')
+
+
+def load_db_test():
+    return TinyDB('scripts/test.json')
+
 
 def main():
     db = load_db()
+    # db = load_db_test()
     # db.purge_tables()
     db.purge_table('questions')
     questions = db.table('questions')
 
     for idx, row in data.questions.iterrows():
-        questions.insert({
-            'section': row['section'], 
-            'question_type': row['question_type'], 
-            'question':row['questions']
-            })
+        question = {
+            'section': row['section'],
+            'question_type': row['question_type'],
+            'question': row['questions']
+        }
+        questions.insert(question)
 
     db.close()
 
