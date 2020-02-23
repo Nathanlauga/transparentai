56edcf2d15e52093e5c3a7d2f6d676284a39ca0d
Nathan <nathan.lauga@gmail.com>
2020-02-23 21:37:08 +0100

    Update: docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,8 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
