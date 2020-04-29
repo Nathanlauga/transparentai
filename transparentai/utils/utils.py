5804278f46bb747584268df7530c6802c99ea62e
Nathan <nathan.lauga@gmail.com>
2020-04-29 10:06:19 +0200

    Add: feature check packages security

--- transparentai/utils/utils.py
+++ transparentai/utils/utils.py
@@ -1,4 +1,5 @@
 import energyusage
+import subprocess
 
 
 def evaluate_kWh(func, *args, verbose=False):
@@ -33,3 +34,22 @@ def evaluate_kWh(func, *args, verbose=False):
                                               pdf=False, printToScreen=verbose)
 
     return kWh, fun_return
+
+
+def check_packages_security(full_report=True):
+    """Using safety package, check out the known vulnerabilities 
+    of the installed packages.
+
+    For more details you can look at the package page :
+    https://github.com/pyupio/safety
+
+    Parameters
+    ----------
+    full_report: True
+        Whether you want the full report or 
+        short report.    
+    """
+    report = '--full-report' if full_report else '--short-report'
+    result = subprocess.run(
+        ['safety', 'check', report], stdout=subprocess.PIPE)
+    print(result.stdout.decode('utf-8'))
